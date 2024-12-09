4563e29a60ddbc6bda3ee9fc0b43f0b2dba69d1c
Bogusdrac <bogusdracula7@gmail.com>
2024-12-09 23:06:06 +0200

    tailwind and react setup

---
+++ artchaar_app/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <>
+      
+      <p className="text-green-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
